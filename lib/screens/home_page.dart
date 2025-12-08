d5adb72b4ef5b63975ad0d91c0bf60808e8ef3c9
romanks866 <romanks866@gmail.com>
2025-12-08 22:15:43 +0000

    Update

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/models/task_model.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Row(children:[
+          Expanded(child:
+          Image.asset('assets/rdplogo.png',height:80)
+          ),
+          Text('Daily Planner',
+          style:TextStyle(
+            fontFamily:'Caveat',
+            fontSize: 32,
+            color: Colors.white
+
+          )),
+           ],)
+    ),
+    drawer:Drawer(),
+    body: Column(children: [
+      TableCalendar(
+      calendarFormat: CalendarFormat.month,
+      focusedDay: DateTime.now(),
+      firstDay: DateTime(2025),
+      lastDay: DateTime(2027),
+      ),
+
+
+    ],)
+      
+    );
+  }
+}
